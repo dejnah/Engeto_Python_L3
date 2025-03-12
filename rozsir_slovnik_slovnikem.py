586e281fcce6896922e8e0cff737109cf4ac23b6
dejnah <168757297+dejnah@users.noreply.github.com>
2025-03-12 18:44:03 +0100

    Úkol - rozsir_slovnik_slovnikem

--- rozsir_slovnik_slovnikem.py
+++ rozsir_slovnik_slovnikem.py
@@ -1,1 +1,17 @@
+# V této úloze vytvoř program, který:
+# Vytvoří nový prázdný slovník user_1
+# doplní do slovníku user_1 klíč name (hodnota "Marek") a surname (hodnota "Parek")
+# pomocí vhodné metody rozšíří stávající slovník user_1 o zadanou proměnnou user_email
+# vypiš hodnoty nového slovníku user_1 s úvodním textem "User #01:"
 
+user_1 = {}
+print(user_1)
+print(type(user_1))
+
+user_1["name"] = "Marek"
+user_1["surname"] = "Parek"
+print(user_1)
+user_email = {"email": "marek.parek@email.com"}
+user_1.update(user_email)
+print(user_1)
+print("User #01:", user_1)
