8a98c90d6de52f6c5353b22d02baea77f6e7e4f3
dejnah <168757297+dejnah@users.noreply.github.com>
2025-03-12 21:23:54 +0100

    Úkol - rozdil_setu

---
+++ rozdil_setu.py
@@ -0,0 +1,12 @@
+cisla_1 = {1, 2, 3, 4}
+cisla_2 = {3, 4, 5, 6}
+
+# Zapiš všechny proměnné výš
+# zjistí jaké hodnoty prvního setu jsou rozdílné oproti setu druhému a uloží hodnoty do proměnné rozdil_cisel
+# vypíše výsledek s ohlášením "Rozdílné hodnoty prvního setu oproti druhému:".
+
+cisla_1 = {1, 2, 3, 4}
+cisla_2 = {3, 4, 5, 6}
+
+rozdil_cisel = cisla_1.difference(cisla_2)
+print("Rozdílné hodnoty prvního setu oproti druhému:", rozdil_cisel)
